c00b60571ebdc4afdc94116e89ce1d26aa101126
SaintVKs <l2enemy.ru@gmail.com>
2023-11-25 13:03:25 +0200

    autoload update

--- index.php
+++ index.php
@@ -1,8 +1,2 @@
 <?php
-
-use Root\Html\Test;
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$sum = new Test();
-d($sum->getSum($sum->numberOne(), $sum->numberTwo(), $sum->numberThree()));
+//next DZ
