f5189aa57b657dad21a96cd6a1c0a8f4fb4b42ce
SaintVKs <l2enemy.ru@gmail.com>
2023-11-27 11:30:10 +0200

    next dz

--- index.php
+++ index.php
@@ -1,14 +1,2 @@
 <?php
-
-use Root\Html\User;
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$account = new User('Алексей', 28, '12345678@gmail.com');
-
-//Проверка несуществующего метода.
-//$account->setNumber('080031031293');
-
-//Проверка существуещего метода.
-//$account->setName('Владислав');
-d($account->getAll());
+//новое дз
